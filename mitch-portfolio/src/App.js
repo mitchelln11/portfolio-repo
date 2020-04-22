395fc45efc375483e7c5a40308edfa5c1efb7e3a
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-04-22 16:58:59 -0500

    footer added and implemented

--- mitch-portfolio/src/App.js
+++ mitch-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Header />
 
         <About />
+        <Footer />
     </div>
   );
 }
---
+++ mitch-portfolio/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const footerBlock = () => {
+    return (
+        <div className="footer-block">
+            <ul>
+                <li>Mitchell Naleid - Web Developer</li>
+                <li><a href="tel:+12623648896"><i class="fas fa-mobile-alt"></i> 1-262-364-8896</a></li>
+                <li><i class="fas fa-map-marker-alt"></i> Menomonee Falls, WI</li>
+            </ul>
+        </div>
+    )
+}
+
+export default footerBlock;

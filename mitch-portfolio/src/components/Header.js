bedf829d57ccb3f94f0cc1eccec4616bca4da62e
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-04-22 15:22:14 -0500

    added header component and implemented

---
+++ mitch-portfolio/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const headerBlock = () => {
+    return (
+        <div className="header-overlay">
+            <h1>Mitchell Naleid</h1><h2>Web Developer</h2>
+        </div>
+    )
+}
+
+export default headerBlock;

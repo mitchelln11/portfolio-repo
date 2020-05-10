94f31c74ddb1c486931cdcd59a122094b63508df
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-05-10 12:54:35 -0500

    got to learn images scrolling through and resetting at the first. Still want to get fade working

--- mitch-portfolio/src/components/ToLearn.js
+++ mitch-portfolio/src/components/ToLearn.js
@@ -2,18 +2,12 @@ import React, { useState, useEffect } from 'react';
 
 function ToLearn() {
     let [items, setItems] = useState([]);
-
     
     // SET CAROUSEL DEFAULTS
     let [currentIndex, setCurrentIndex] = useState(0); // Initial slide index value
-    let currentLogo = items[currentIndex]; // variable index value we can reference later
+    let currentLogo = items[currentIndex]; // variable index value we can reference later 
     
-    const toLearnImgs = () => {
-        currentIndex !== items.length -1 ? // Check index length
-        setCurrentIndex(currentIndex + 1) : setCurrentIndex(currentIndex = 0);
-        currentLogo = items[currentIndex];
-    }
-
+    // Make API call
     useEffect(() => {
         const fetchItems = async () => {
             const url = "http://localhost:3002/learn";
@@ -23,14 +17,21 @@ function ToLearn() {
         }
         fetchItems();
     }, []);
-    
 
+    // Set timer for updating image
+    useEffect(() => {
+        const imgInterval = setInterval(() => {
+            currentIndex !== items.length -1 ? // Check index length
+            setCurrentIndex(currentIndex + 1) : setCurrentIndex(currentIndex = 0);
+            currentLogo = items[currentIndex];
+        }, 8000);
+        return () => clearInterval(imgInterval); // Unmounting???
+    }, [currentIndex]); // Run every time the index changes
 
     // Return will run through once before it makes the fetch call, so it will be null. After the component mounts, values will come in.
     if (!items.length) {
         return null;
     }
-
 
     return (
         <div className="flex-child languages">

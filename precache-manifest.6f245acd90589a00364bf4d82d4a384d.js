0e178a7e71a80657337d62cca99f49661e20b6bc
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-05-13 11:25:18 -0500

    Updates

---
+++ precache-manifest.6f245acd90589a00364bf4d82d4a384d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118d3b7d52377806dd098ee2a36c40b5",
+    "url": "/mitch-portfolio/index.html"
+  },
+  {
+    "revision": "00132a64931e6a29e645",
+    "url": "/mitch-portfolio/static/css/main.c45321e0.chunk.css"
+  },
+  {
+    "revision": "78d3425d84343560bb26",
+    "url": "/mitch-portfolio/static/js/2.a2fb9dec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mitch-portfolio/static/js/2.a2fb9dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00132a64931e6a29e645",
+    "url": "/mitch-portfolio/static/js/main.a6faffff.chunk.js"
+  },
+  {
+    "revision": "5c9fa96e6e00916429bd",
+    "url": "/mitch-portfolio/static/js/runtime-main.83cadf4e.js"
+  }
+]);

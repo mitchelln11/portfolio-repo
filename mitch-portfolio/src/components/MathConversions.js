bf845d1bc4a1db3f14c6947328db9275c6ea7141
Mitchell Naleid <mitchellnaleid@gmail.com>
2020-06-12 21:56:48 -0500

    ternary operator to prevent NaN from briefly showing up

--- mitch-portfolio/src/components/MathConversions.js
+++ mitch-portfolio/src/components/MathConversions.js
@@ -1,10 +1,8 @@
 import React from 'react';
 
 const KelvinConvert = ({kelvin}) => {
-
   const convertKelvinToFahrenheit = kelvin => (kelvin - 273.15) * 9/5 + 32;
-  return <li>{convertKelvinToFahrenheit(kelvin).toFixed(2)}&deg;F</li>;
-
+  return kelvin ? <li>{convertKelvinToFahrenheit(kelvin).toFixed(2)}&deg;F</li> : "";
 }
 
 export default KelvinConvert;
